f04107d0f9d20d5e32e0fce0158fd8d2b7e49488
Roberto Londoño <roberthdrums@gmail.com>
2024-05-13 18:48:22 -0500

    update docker

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,5 @@ RUN gradle build -x test
 
 FROM amazoncorretto:11.0.14-alpine3.15
 ENV TZ="America/Bogota"
-COPY --from=builder build/libs/authentication-api-0.0.1-SNAPSHOT.jar app.jar
+COPY --from=builder build/libs/authentication-api-1.0.0.jar app.jar
 CMD [ "java", "-jar", "app.jar" ]
--- src/main/java/com/app/authentication/util/CreateRoles.java
+++ src/main/java/com/app/authentication/util/CreateRoles.java
@@ -1,33 +1,54 @@
 package com.app.authentication.util;
 
+import com.app.authentication.dtos.request.UserSignUpRequestDto;
 import com.app.authentication.enums.RolName;
 import com.app.authentication.models.RolModel;
 import com.app.authentication.services.RolService;
+import com.app.authentication.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Component
 public class CreateRoles implements CommandLineRunner {
 
   @Autowired
   RolService rolService;
+  @Autowired
+  UserService userService;
 
   private final static Logger _log = LoggerFactory.getLogger(CreateRoles.class);
 
-
   @Override
-  public void run(String... args) throws Exception {
+  public void run(String... args) {
     RolName[] rolNames = RolName.values();
-    for (int i = 0; i < rolNames.length; i++) {
-      if(!rolService.existsByRolName(rolNames[i])){
-        _log.info("Rol to create: "+rolNames[i].toString());
-        RolModel rolToSave = new RolModel(rolNames[i]);
+    for (RolName rolName : rolNames) {
+      if (!rolService.existsByRolName(rolName)) {
+        _log.info("Rol to create: {}", rolName.toString());
+        RolModel rolToSave = new RolModel(rolName);
         rolService.save(rolToSave);
       }
     }
+    boolean existsByEmail = userService.existsByEmail("roberthdrums@gmail.com");
+    if (!existsByEmail) {
+      _log.info("User to create: roberthdrums@gmail.com");
+      UserSignUpRequestDto userRequestDto = new UserSignUpRequestDto();
+      Set<String> roles = new HashSet<>();
+      roles.add("ADMIN");
+      userRequestDto.setRoles(roles);
+      userRequestDto.setPassword("123456");
+      userRequestDto.setName("Roberto");
+      userRequestDto.setPhone("12345678");
+      userRequestDto.setLastName("Londo\u00f1o");
+      userRequestDto.setEmail("roberthdrums@gmail.com");
+      userService.createUser(userRequestDto);
+    }
+
   }
 
 }
